d5fc3154eee303ff10b50b07cb5f6080a42956f7
tonamichan <sunday.gamer.tonami.ch@gmail.com>
2023-10-24 16:53:28 +0900

    test

--- main.vue
+++ main.vue
@@ -1,11 +1,20 @@
 <template>
     <div>ここにタスクリストを出すよ～</div>
+    <ul id="example-1">
+      <li v-for="tasks in task" :key="task.id">
+        {{ task.id }}: {{ task.name }}
+      </li>
+    </ul>
   </template>
   
   <script>
   module.exports = {
     data() {
       return {
+        tasks: [
+          {id: 1, name: 'されわもってこい', who: 'せらぴ', parent: []},
+          {id: 2, name: 'すかぶたおせ', who: 'ぷらぱ', parent: []},
+        ]
       }
     },
   }
