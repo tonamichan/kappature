f41af67bc430233424c3deac1d24a7aefc56eef0
HiroyukiTonami <nyamu_z@yahoo.co.jp>
2026-02-24 01:07:24 +0900

    terser使わない

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    minify: 'terser',
+    assetsDir: 'assets'
   },
   server: {
     port: 5173,
