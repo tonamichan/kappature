39c4c264257b72a9ea4c0ede2d4c0c97a998b7fa
tonamichan <sunday.gamer.tonami.ch@gmail.com>
2026-02-24 00:34:51 +0900

    WIP_copilotにvite移行書かせた

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue2'
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'terser',
+  },
+  server: {
+    port: 5173,
+    open: true,
+  }
+})
